b1ad58e696f3b6be705e9323db5ed74547046cf0
kazuma1989 <kazuma.ebina@quipper.com>
2021-04-03 09:44:44 +0900

    feat: Try node-tar package

--- bin.js
+++ bin.js
@@ -5,40 +5,63 @@
 
 const fs = require("fs")
 const https = require("https")
+const tar = require("tar")
+
+/**
+ * @param {string} url
+ * @returns {Promise<import("http").IncomingMessage>}
+ */
+async function fetch(url) {
+  /** @type {import("http").IncomingMessage} */
+  const resp = await new Promise((resolve, reject) => {
+    https.get(url, resolve).on("error", reject)
+  })
+
+  const { statusCode, statusMessage, headers } = resp
+
+  if (statusCode >= 500 || statusCode >= 400) {
+    throw new Error(`${statusCode} ${statusMessage}`)
+  }
+
+  if (statusCode >= 300) {
+    return await fetch(headers.location)
+  }
+
+  return resp
+}
 
 /**
  * @param {string} url
  * @param {string} dest
  */
-async function fetch(url, dest) {
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, (resp) => {
-        const { statusCode, statusMessage, headers } = resp
+async function download(url, dest) {
+  const resp = await fetch(url)
 
-        if (statusCode >= 400) {
-          reject({ statusCode, statusMessage })
-          return
-        }
-
-        if (statusCode >= 300) {
-          fetch(headers.location, dest).then(resolve, reject)
-          return
-        }
-
-        resp
-          .pipe(fs.createWriteStream(dest))
-          .on("finish", () => resolve())
-          .on("error", reject)
-      })
+  await new Promise((resolve, reject) => {
+    resp
+      .pipe(fs.createWriteStream(dest))
+      .on("finish", resolve)
       .on("error", reject)
   })
 }
 
-async function main() {
-  await fetch(
-    "https://github.com/kazuma1989/firebook/archive/HEAD.zip",
-    "./bar.zip"
+/**
+ * @param {string=} subDir
+ */
+async function main(subDir) {
+  await download(
+    "https://github.com/kazuma1989/firebook/archive/HEAD.tar.gz",
+    "./HEAD.tar.gz"
+  )
+
+  const dest = "./"
+  await tar.extract(
+    {
+      file: "./HEAD.tar.gz",
+      strip: subDir ? subDir.split("/").length : 1,
+      cwd: dest,
+    },
+    subDir ? [subDir] : []
   )
 }
 
